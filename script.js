261faba254f3e52994a658e1a2c7babcca361117
Orhan <108482875+Orhan8083@users.noreply.github.com>
2025-01-13 21:12:47 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function canlilar() {
+    var img = document.getElementById('image');
+    img.src = 'x.png';
+}
+
+function tarihialanlar() {
+    var img = document.getElementById('image');
+    img.src = 'y.png';
+}
+
+function dogalalanlar() {
+    var img =document.getElementById('image')
+    img.src = 'z.png'
+}
+
+function gecimkaynaklari() {
+    var img =document.getElementById('image')
+    img.src = 'k.png'
+}
